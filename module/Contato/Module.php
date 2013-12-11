1fdb6ba8158b78c74f57a0dc58daac548127b5d3
Thiago Neves <thiagonevesuf@gmail.com>
2013-12-11 21:06:22 -0200

    Teste Zend 2 - segundo commit Criação do branch modulo_contato

---
+++ module/Contato/Module.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * namespace para nosso modulo contato
+ */
+namespace Contato;
+ 
+class Module
+{
+    /**
+     * include de arquivo para outras configuracoes desse modulo
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+ 
+    /**
+     * autoloader para nosso modulo
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
