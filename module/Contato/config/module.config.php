4d286f3d54a275903697d5fae82a462541656783
Thiago Neves <thiagonevesuf@gmail.com>
2013-12-11 21:38:25 -0200

    Teste Zend 2 - terceiro commit Criação do branch controller contato *página sobre criada

--- module/Contato/config/module.config.php
+++ module/Contato/config/module.config.php
@@ -11,6 +11,8 @@ return array(
     # definir e gerenciar rotas
     'router' => array(
         'routes' => array(
+            
+            # literal para action index home
             'home' => array(
                 'type'      => 'Literal',
                 'options'   => array(
@@ -18,6 +20,18 @@ return array(
                     'defaults' => array(
                         'controller' => 'HomeController',
                         'action'     => 'index',
+                    ),
+                ),
+            ),
+
+            # literal para action sobre home
+            'sobre' => array(
+                'type'      => 'Literal',
+                'options'   => array(
+                    'route'    => '/sobre',
+                    'defaults' => array(
+                        'controller' => 'HomeController',
+                        'action'     => 'sobre',
                     ),
                 ),
             ),
